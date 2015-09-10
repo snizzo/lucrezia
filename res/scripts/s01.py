886383f262f1bfa909bd40166a749d948d89961e
Claudio Desideri <happy.snizzo@gmail.com>
2015-09-10 18:34:28 +0200

    added open/close animation to baloons

--- res/scripts/s01.py
+++ res/scripts/s01.py
@@ -1,4 +1,4 @@
 baloons.push('Chiara', 'Ehi! Non toccare la mia pianta!', 'chiara')
 baloons.push('Ellen', 'Scusa...', 'ellen')
 ###pause
-audioManager.playMusic('soundtrack/s01.ogg')
+#audioManager.playMusic('soundtrack/s01.ogg')
--- src/gui/baloon.py
+++ src/gui/baloon.py
@@ -24,20 +24,34 @@ class Baloon(DirectObject):
         self.message = list(message[::-1])
         self.textapplied = [who,':\n']
         self.speed = speed
+        
+        #good default?
+        self.openCloseSpeed = 0.2
     
     def requestPause(self):
         messenger.send("pauseGameplay");
     
     def resumePause(self):
+        closeInterval = self.textnp.scaleInterval(self.openCloseSpeed, 0.0001, 0.37)
+        Sequence(
+            closeInterval,
+            Func(messenger.send, "resumeGameplay"),
+            Func(self.ignoreAll),
+            Func(self.textnp.remove_node)
+        ).start()
+        
+        '''
         messenger.send("resumeGameplay");
         self.ignoreAll()
         self.textnp.remove_node()
+        '''
         
     def canResumePause(self):
         self.accept("space", self.resumePause);
     
     def textAnimation(self):
         taskMgr.doMethodLater(self.speed, self.addLetter, 'showletters')
+        self.textnp.scaleInterval(self.openCloseSpeed, 0.37, 0.0001).start()
         
     def addLetter(self, Task):
         self.textapplied.append(self.message.pop())
@@ -54,27 +68,27 @@ class Baloon(DirectObject):
         
         #text
         self.textbg = TextNode('baloontextnodebg')
-        self.textbg.setTextColor(0.5, 0.5, 0.5, 1)
-        self.textbg.setWordwrap(15.0)
+        self.textbg.setTextColor(0.5, 0.5, 0.5, 0)
+        self.textbg.setWordwrap(13.0)
         self.textbg.setText(''.join([self.who,":\n"]+self.message))
         
         #card as background
-        self.textbg.setFrameColor(0.7, 0.7, 0.7, 0.7)
-        self.textbg.setFrameAsMargin(0.4, 0.4, 0.4, 0.4)
-        self.textbg.setCardColor(0.5, 0.5, 0.5, 1)
-        self.textbg.setCardAsMargin(0.4, 0.4, 0.4, 0.4)
+        self.textbg.setFrameColor(0.7, 0.7, 0.7, 0.6)
+        self.textbg.setFrameAsMargin(0.4, 0.4, 0.4, 0.3)
+        self.textbg.setCardColor(0.5, 0.5, 0.5, 0.67)
+        self.textbg.setCardAsMargin(0.4, 0.4, 0.4, 0.3)
         self.textbg.setCardDecal(True)
         
         #text
         self.text = TextNode('baloontextnode')
         self.text.setTextColor(1, 1, 1, 1)
-        self.text.setWordwrap(15.0)
+        self.text.setWordwrap(13.0)
         
         self.textnp = render.attachNewNode(self.text)
         textradius = self.textnp.getBounds().getRadius()/2
         self.textnp.setY(-0.5)
-        self.textnp.setScale(0.37)
-        self.textnp.setPos(self.pos.getX(),-1,self.pos.getZ()+2)
+        self.textnp.setScale(0.0001)
+        self.textnp.setPos(self.pos.getX()+0.5,-1,self.pos.getZ()+2.25)
         self.textnp.setLightOff()
         self.textbgnode = self.textnp.attachNewNode(self.textbg)
         self.textbgnode.setY(0.1)
