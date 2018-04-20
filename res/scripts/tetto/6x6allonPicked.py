05720270a7d36890f1ef3b35cac6a11e3f66baa2
Claudio Desideri <happy.snizzo@gmail.com>
2018-04-20 11:47:13 +0200

    Demo updated

--- res/scripts/tetto/6x6allonPicked.py
+++ res/scripts/tetto/6x6allonPicked.py
@@ -1,1 +1,16 @@
-baloons.push('Lear', 'ciao', 'mainplayer')
+lear = pGrid.getObjectById('mainplayer')
+lear.setPlayable(False)
+baloons.push('Lear', 'Eccoti finalmente!', 'mainplayer')
+baloons.push('Lear', 'È un\'ora che ti cerco.', 'mainplayer')
+###pause
+luna = pGrid.getObjectById('luna')
+customCamera.moveCameraAtObject(luna)
+###pause
+baloons.push('Luna', 'Non sapevo dove altro andare.', 'luna')
+###pause
+lear = pGrid.getObjectById('mainplayer')
+customCamera.moveCameraAtObject(lear)
+###pause
+lear = pGrid.getObjectById('mainplayer')
+baloons.push('Lear', 'Ti stanno aspettando i dottori!', 'mainplayer')
+lear.setPlayable(True)
